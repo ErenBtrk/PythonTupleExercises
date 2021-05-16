5842f0cf7ac263badf924eaa29fc1ad573fb11d3
ErenBtrk <erenbtrk@hotmail.com>
2021-05-16 20:38:50 +0300

    Exercise 10

---
+++ Exercise10.py
@@ -0,0 +1,10 @@
+'''
+10. Write a Python program to check whether an element exists within a tuple.
+'''
+
+tuple1 = (1,2,3,4,5,6)
+
+if 7 in tuple1:
+    print("Its in the tuple")
+else:
+    print("Its not in the tuple")
