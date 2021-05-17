ec89639e2ef54e3c38b61c3922f8879da2666c00
ErenBtrk <erenbtrk@hotmail.com>
2021-05-17 10:42:03 +0300

    Exercise 14

--- Exercise14.py
+++ Exercise14.py
@@ -2,6 +2,8 @@
 14. Write a Python program to find the index of an item of a tuple.
 '''
 
-tuple1 = (1,2,3,4,5)
+tuple1 = (1,2,3,4,5,6,7,8,9,10)
 
 print(tuple1.index(4))
+
+print(tuple1.index(8,5,9))
---
+++ Exercise15.py
@@ -0,0 +1,8 @@
+'''
+15. Write a Python program to find the length of a tuple.
+'''
+
+tuple1 = (1,2,3,4,5)
+
+print(len(tuple1))
+
