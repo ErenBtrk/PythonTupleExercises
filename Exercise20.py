6be48319b1d4ca963492ef322a0938bdff7ec4b2
ErenBtrk <erenbtrk@hotmail.com>
2021-05-17 11:23:48 +0300

    Exercise 20

---
+++ Exercise20.py
@@ -0,0 +1,10 @@
+'''
+20. Write a Python program to print a tuple with string formatting. 
+Sample tuple : (100, 200, 300)
+Output : This is a tuple (100, 200, 300)
+'''
+
+tuple1 = (100,200,300)
+
+print(f"This is a tuple {tuple1}")
+
